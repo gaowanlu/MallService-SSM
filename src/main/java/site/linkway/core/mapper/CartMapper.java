7dc49ec67603a18b71fdb3d0549ca2fd242f134e
gaowanlu <2209120827@qq.com>
2021-11-29 21:34:28 +0800

    delete about smaple* everything

--- src/main/java/site/linkway/core/mapper/CartMapper.java
+++ src/main/java/site/linkway/core/mapper/CartMapper.java
@@ -1,9 +1,11 @@
 package site.linkway.core.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
 import site.linkway.core.entity.po.Cart;
 import java.util.List;
 
 //table name:cart  entity:po.Cart
+@Mapper
 public interface CartMapper {
     int insert(Cart cart);
     int update(Cart cart);//只负责更新数量
--- src/main/java/site/linkway/core/mapper/FCommentMapper.java
+++ src/main/java/site/linkway/core/mapper/FCommentMapper.java
@@ -1,8 +1,10 @@
 package site.linkway.core.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
 import site.linkway.core.entity.po.FComment;
 
 //table name:fComment  entity:po.FComment
+@Mapper
 public interface FCommentMapper {
     int insert(FComment fComment);
     int update(FComment fComment);
