356194d1e4128e4356b2807672bfb65404e9caff
gaowanlu <2209120827@qq.com>
2021-12-27 22:44:29 +0800

    添加utils/* 注释

--- src/main/java/site/linkway/utils/ResponseUtils.java
+++ src/main/java/site/linkway/utils/ResponseUtils.java
@@ -3,6 +3,10 @@ package site.linkway.utils;
 import javax.servlet.http.HttpServletResponse;
 
 public class ResponseUtils {
+    /**
+     * 为Content-Type设置 text/json;charset=utf-8
+     * @param httpServletResponse
+     */
     public static void ContentTypeJSONUTF8(HttpServletResponse httpServletResponse){
         httpServletResponse.setCharacterEncoding("UTF-8");
         httpServletResponse.setHeader("content-type","text/json;charset=utf-8");
