474d624d9cc898ff7ffbad25bbfca03d94e9d845
fronz <z233@msn.com>
2021-12-27 21:26:13 +0800

    Refactor cart module

--- src/main/java/site/linkway/core/controller/ShoppingCart.java
+++ src/main/java/site/linkway/core/controller/ShoppingCart.java
@@ -8,8 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import site.linkway.core.entity.bo.PostCart;
-import site.linkway.core.entity.po.Cart;
-import site.linkway.core.entity.po.CartItem;
 import site.linkway.core.entity.vo.CartList;
 import site.linkway.core.entity.vo.StatusResult;
 import site.linkway.core.service.ShoppingCartService;
@@ -21,7 +19,7 @@ import javax.servlet.http.HttpSession;
 @Controller
 @RequestMapping("/api/user")
 public class ShoppingCart {
-    static Logger logger= Logger.getLogger(ShoppingCart.class);
+    static Logger logger = Logger.getLogger(ShoppingCart.class);
     private ObjectMapper mapper = new ObjectMapper();
 
     @Autowired
@@ -34,62 +32,79 @@ public class ShoppingCart {
     @Autowired
     HttpSession httpSession;
 
-    /*新增购物车条项*/
-    @PostMapping (value = "/cart",produces = "application/json;charset=utf-8")
+    /**
+     * 向购物车添加物品
+     *
+     * @param email  HttpSession 中的用户邮箱
+     * @param goodId 商品 id
+     * @param num    商品数量
+     * @return 添加结果
+     * @throws JsonProcessingException
+     */
+    @PostMapping(value = "/cart", produces = "application/json;charset=utf-8")
     @ResponseBody
-    public String addCart(@NonNull String goodId,
-                          @SessionAttribute(value = "id") String email,
+    public String addCart(@SessionAttribute(value = "id") String email,
+                          @NonNull String goodId,
                           @NonNull int num) throws JsonProcessingException {
-        StatusResult statusResult=new StatusResult();
-        //添加购物车条项
-        statusResult.setResult(shoppingCartService.addCart(goodId,email,num));
+        StatusResult statusResult = new StatusResult();
+        // 添加购物车条项并写入结果
+        statusResult.setResult(shoppingCartService.addCart(goodId, email, num));
         return mapper.writeValueAsString(statusResult);
     }
 
-    /*删除购物车条项 同时返回现有条项*/
-    @DeleteMapping(value = "/cart",produces = "application/json;charset=utf-8")
+    /**
+     * 删除购物车中的物品
+     *
+     * @param email  HttpSession 中的用户邮箱
+     * @param cartId 购物车 id
+     * @return 删除之后购物车的内容
+     * @throws JsonProcessingException
+     */
+    @DeleteMapping(value = "/cart", produces = "application/json;charset=utf-8")
     @ResponseBody
     public String addCart(@NonNull String cartId,
                           @SessionAttribute(value = "id") String email
-                          ) throws JsonProcessingException {
-        CartList cartList=new CartList();
-        //删除购物车条项 result为此次操作是否成功
-        cartList.result=shoppingCartService.deleteCart(cartId,email);
-        //获取此用户的购物车条项
+    ) throws JsonProcessingException {
+        CartList cartList = new CartList();
+        // 删除购物车条项并写入操作结果
+        cartList.result = shoppingCartService.deleteCart(cartId, email);
+        // 获取此用户的购物车条项
         cartList.setCarts(shoppingCartService.getCartsByEmail(email));
-        //结果返回
+        // 结果返回
         return mapper.writeValueAsString(cartList);
     }
 
-    /*获取购物车全部条项*/
-    @GetMapping(value = "/cart",produces = "application/json;charset=utf-8")
+    /**
+     * 获取购物车的全部内容
+     *
+     * @param email HttpSession 中的用户邮箱
+     * @return 购物车的全部内容
+     * @throws JsonProcessingException
+     */
+    @GetMapping(value = "/cart", produces = "application/json;charset=utf-8")
     @ResponseBody
     public String addCart(@SessionAttribute(value = "id") String email) throws JsonProcessingException {
-        CartList cartList=new CartList();
-        //获取购物车条项
+        CartList cartList = new CartList();
+        // 获取购物车条项
         cartList.setCarts(shoppingCartService.getCartsByEmail(email));
         return mapper.writeValueAsString(cartList);
     }
 
-    /*购物车条项更新 客户端将购物车条项修改后再进行提交 进行更新处理*/
-    @PutMapping(value = "/cart",produces = "application/json;charset=utf-8")
+    /**
+     * 提交购物车
+     *
+     * @param postCart 购物车信息
+     * @return 提交结果
+     * @throws JsonProcessingException
+     */
+    @PutMapping(value = "/cart", produces = "application/json;charset=utf-8")
     @ResponseBody
     public String updateCart(@SessionAttribute("id") String email,
-                             @RequestBody CartItem cart) throws JsonProcessingException {
-        /*只更新 数量*/
-        boolean result=shoppingCartService.updateCart(email,cart);
-        StatusResult statusResult=new StatusResult();
-        statusResult.setResult(result);
-        return mapper.writeValueAsString(statusResult);
-    }
-
-    /*购物车覆盖更新*/
-    @PutMapping(value="/cart/cover",produces = "application/json;charset=utf-8")
-    @ResponseBody
-    public String coverCart(@SessionAttribute("id") String email,
-                            @RequestBody PostCart postCart[]) throws JsonProcessingException {
-        boolean result= shoppingCartService.cover(email,postCart);
-        CartList cartList=new CartList();
+                             @RequestBody PostCart postCart[]) throws JsonProcessingException {
+        // 使用提交的购物车条项更新旧的购物车数据
+        boolean result = shoppingCartService.update(email, postCart);
+        // 返回操作结果
+        CartList cartList = new CartList();
         cartList.setResult(result);
         cartList.setCarts(shoppingCartService.getCartsByEmail(email));
         return mapper.writeValueAsString(cartList);
--- src/main/java/site/linkway/core/service/ShoppingCartServiceImpl.java
+++ src/main/java/site/linkway/core/service/ShoppingCartServiceImpl.java
@@ -20,7 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class ShoppingCartServiceImpl implements ShoppingCartService{
+public class ShoppingCartServiceImpl implements ShoppingCartService {
     @Autowired
     CartMapper cartMapper;
     @Autowired
@@ -30,67 +30,88 @@ public class ShoppingCartServiceImpl implements ShoppingCartService{
     @Autowired
     GoodImgMapper goodImgMapper;
 
-    //添加购物车条项
+    /**
+     * 向购物车中添加商品
+     *
+     * @param goodId 商品 id
+     * @param email  用户邮箱
+     * @param num    商品数量
+     * @return 是否添加成功
+     */
     @Override
     public boolean addCart(String goodId, String email, int num) {
-        String userId=userMapper.selectIdByEmail(email);
-        Cart inertCart=new Cart(UUIDUtils.getUUID(),goodId,userId,num);
-        return 1==cartMapper.insert(inertCart);
+        String userId = userMapper.selectIdByEmail(email);
+        Cart inertCart = new Cart(UUIDUtils.getUUID(), goodId, userId, num);
+        return 1 == cartMapper.insert(inertCart);
     }
 
-    //删除购物车条项
+    /**
+     * 删除购物车中的商品
+     *
+     * @param cartId 购物车 id
+     * @param email  用户邮箱
+     * @return 是否删除成功
+     */
     @Override
     public boolean deleteCart(String cartId, String email) {
-        //根据邮箱寻找用户
-        User user=new User();user.setEmail(email);
-        User targetUser=userMapper.select(user);
-        Cart delCart=new Cart();
+        // 根据邮箱用户
+        User user = new User();
+        user.setEmail(email);
+        User targetUser = userMapper.select(user);
+        Cart delCart = new Cart();
         delCart.setCartId(cartId);
         delCart.setUserId(targetUser.getUserId());
-        return 1==cartMapper.delete(delCart);
+        return 1 == cartMapper.delete(delCart);
     }
 
-    //根据邮箱检索出用户的所有购物车条项
+    /**
+     * 获取指定用户的购物车
+     *
+     * @param email 用户邮箱
+     * @return 购物车列表
+     */
     @Override
     public List<CartItem> getCartsByEmail(String email) {
-        //根据邮箱寻找用户
-        User user=new User();user.setEmail(email);
-        User targetUser=userMapper.select(user);
-        Cart cart=new Cart();
+        // 根据邮箱寻找用户
+        User user = new User();
+        user.setEmail(email);
+        User targetUser = userMapper.select(user);
+        Cart cart = new Cart();
         cart.setUserId(targetUser.getUserId());
-        /*根据购物车条项检索出物品详情*/
-        List<CartItem> cartItemList=cartMapper.selectCartsDetail(email);
-        /*需要根据每个商品将其相关图片检索出来*/
-        for(CartItem item:cartItemList){
-            //根据goodId检索出imgId
-            List<String> goodIds=goodImgMapper.selectImgIdByGoodId(item.getGoodId());
-            for(int i=0;i<goodIds.size();i++){
-                String URL=goodIds.get(i);
-                URL=ImageDistribution.formatURLFromImgId(URL);
-                goodIds.set(i,URL);
+        // 根据用户 id 查询购物车
+        List<CartItem> cartItemList = cartMapper.selectCartsDetail(email);
+        // 为购物车当中的商品添加图片
+        for (CartItem item : cartItemList) {
+            // 根据 goodId 检索出 imgId
+            List<String> goodIds = goodImgMapper.selectImgIdByGoodId(item.getGoodId());
+            for (int i = 0; i < goodIds.size(); i++) {
+                String URL = goodIds.get(i);
+                URL = ImageDistribution.formatURLFromImgId(URL);
+                goodIds.set(i, URL);
             }
+            // 将 imgsURL 添加到 item 中
             item.setImgsURL(goodIds);
         }
         return cartItemList;
     }
 
-    //更新购物车条项，实际只有num字段需要更新
+    /**
+     * 提交购物车
+     * 覆盖式更新
+     *
+     * @param email    用户邮箱
+     * @param postCart 购物车
+     * @return 是否提交成功
+     */
     @Override
-    public boolean updateCart(String email,CartItem cartItem) {
-        String userId=userMapper.selectIdByEmail(email);
-        return 1==cartMapper.update(cartItem.cartId,cartItem.num,userId);
-    }
-
-    /*购物车条项，覆盖更新*/
-    @Override
-    @Transactional
-    public boolean cover(String email,PostCart[] postCart) {
-        String userId=userMapper.selectIdByEmail(email);
-        /*删除此用户的购物车所有项*/
+    @Transactional // 事务注解，提交失败则回滚
+    public boolean update(String email, PostCart[] postCart) {
+        String userId = userMapper.selectIdByEmail(email);
+        // 删除原有购物车
         cartMapper.deleteByUserId(userId);
-        /*重新插入*/
-        for(PostCart item:postCart){
-            if(false==addCart(item.getGoodId(),email, item.getNum())){
+        // 插入新的购物车
+        for (PostCart item : postCart) {
+            if (false == addCart(item.getGoodId(), email, item.getNum())) {
                 return false;
             }
         }
